1a30fe1daf23cefb4ef22b18f8e9d32ba9f7fc0d
Ricky.You <1350753448@qq.com>
2024-01-17 14:34:17 +0000

    initial commit

---
+++ src/main/java/teama/hackathon/ai_hackathon_drug_service/AiHackathonDrugServiceApplication.java
@@ -0,0 +1,13 @@
+package teama.hackathon.ai_hackathon_drug_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHackathonDrugServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiHackathonDrugServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teama/hackathon/ai_hackathon_drug_service/AiHackathonDrugServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package teama.hackathon.ai_hackathon_drug_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHackathonDrugServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
