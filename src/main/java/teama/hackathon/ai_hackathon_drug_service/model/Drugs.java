9d20497cf965f2dcd19cb9cb1b6c78e7341aab4c
Steve Marks <stephen.1.marks@ba.com>
2024-01-17 14:55:33 +0000

    Added Controller

---
+++ src/main/java/teama/hackathon/ai_hackathon_drug_service/model/Drugs.java
@@ -0,0 +1,22 @@
+package teama.hackathon.ai_hackathon_drug_service.model;
+
+public class Drugs {
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
